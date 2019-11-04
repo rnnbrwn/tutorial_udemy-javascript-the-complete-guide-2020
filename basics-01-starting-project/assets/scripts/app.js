c9c494d374c26a31f575edfc86ac051e3ef52339
Ronnie <ronniebrown@gmail.com>
2019-11-04 16:34:21 +0000

    Using constants

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,7 +1,9 @@
+const defaultResult = 0;
+
 let currentResult = 0;
 
 currentResult = currentResult + 10;
 
-let calculatorDescription = currentResult + ' + 10';
+let calculatorDescription = defaultResult + ' + 10';
 
 outputResult(currentResult, calculatorDescription);
