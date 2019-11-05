64030ab3acfeee36f6d03fb92caca5c95d440e41
Ronnie Brown <ronnie.brown@hilton.com>
2019-11-05 10:59:25 +0000

    Creating objects

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -16,8 +16,14 @@ function add() {
 	const initialResult = currentResult;
 	currentResult += enteredNumber;
 	createAndWriteLog('+', initialResult, enteredNumber);
-	logEntries.push(enteredNumber);
-	console.log(logEntries[0]);
+	const logEntry = {
+		operation: 'ADD',
+		prevResult: initialResult,
+		number: enteredNumber,
+		result: currentResult
+	};
+	logEntries.push(logEntry);
+	console.log(logEntries);
 }
 
 function subtract() {
