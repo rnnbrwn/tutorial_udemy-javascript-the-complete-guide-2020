0bfb0b591a120709b3ef679565adaacb27ae30b6
Ronnie <ronniebrown@gmail.com>
2019-11-05 22:35:55 +0000

    Using 'if' statements for checking the win-condition

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -7,8 +8,17 @@ let currentPlayerHealth = chosenMaxLife;
 adjustHealthBars(chosenMaxLife);
 
 function attackHandler() {
-	const damage = dealMonsterDamage(ATTACK_VALUE);
-	currentMonsterHealth -= damage;
+	const monsterDamage = dealMonsterDamage(ATTACK_VALUE);
+	currentMonsterHealth -= monsterDamage;
+	const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+	currentPlayerHealth -= playerDamage;
+	if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+		alert('You won!');
+	} else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+		alert('You lost!');
+	} else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+		alert("It's a draw!");
+	}
 }
 
 attackBtn.addEventListener('click', attackHandler);
